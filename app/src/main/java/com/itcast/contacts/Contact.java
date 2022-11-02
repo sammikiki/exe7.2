636409b797e35126c85e1db538b94c2ff53c15a1
sammi <1767433979@qq.com>
2022-11-02 21:14:28 +0800

    内容提供者

---
+++ app/src/main/java/com/itcast/contacts/Contact.java
@@ -0,0 +1,16 @@
+package com.itcast.contacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.exe7.R;
+
+public class Contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
---
+++ app/src/main/java/com/itcast/contacts/ContactInfo.java
@@ -0,0 +1,19 @@
+package com.itcast.contacts;
+
+
+public class ContactInfo {
+    private String contactName;   //联系人名称
+    private String phoneNumber;   //电话号码
+    public String getContactName() {
+        return contactName;
+    }
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
